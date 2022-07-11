ffa8fa12af7d5567d12c6a83b9893de59a256934
Cameron Ray <cameron@cameronray.net>
2022-07-11 16:34:10 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ use std::ptr::null;
 
 struct Point {
     x: u8,
-    y: u8
+    y: u8,
+}
+
+enum Direction {
+    Vertical,
+    Horizontal,
 }
 
 struct Ship {
     name: String,
     size: u8,
     hits: Vec<bool>,
-    position: Option<Point>
+    position: Option<Point>,
 }
 
 impl Ship {
@@ -19,8 +24,8 @@ impl Ship {
 }
 
 /**
- * 5 ships: 
- * 
+ * 5 ships:
+ *
  * Carrier: 5 holes
  * Battleship: 4 holes
  * Cruiser: 3 holes
@@ -34,35 +39,35 @@ fn default_ships() -> Vec<Ship> {
         name: "Carrier".into(),
         size: 5,
         hits: vec![false; 5],
-        position: None
+        position: None,
     });
 
     ships.push(Ship {
         name: "Battleship".into(),
         size: 4,
         hits: vec![false; 4],
-        position: None
+        position: None,
     });
 
     ships.push(Ship {
         name: "Cruiser".into(),
         size: 3,
         hits: vec![false; 3],
-        position: None
+        position: None,
     });
 
     ships.push(Ship {
         name: "Submarine".into(),
         size: 3,
         hits: vec![false; 3],
-        position: None
+        position: None,
     });
 
     ships.push(Ship {
         name: "Destroyer".into(),
         size: 2,
         hits: vec![false; 2],
-        position: None
+        position: None,
     });
 
     ships
@@ -70,41 +75,48 @@ fn default_ships() -> Vec<Ship> {
 
 struct Field {
     spaces: [[bool; 10]; 10],
-    ships: Vec<Ship>
+    ships: Vec<Ship>,
 }
 
 impl Field {
     fn new() -> Self {
         let spaces = [[false; 10]; 10];
-        
-        // let ships: Vec<Ship> = 
-        return Field { spaces, ships: default_ships() }
+
+        // let ships: Vec<Ship> =
+        return Field {
+            spaces,
+            ships: default_ships(),
+        };
     }
 
     fn shoot(&self, x: usize, y: usize) -> bool {
         self.spaces[x][y]
     }
+
+    fn place_ship(name: String, position: Point) {}
+}
+
+struct User {
+    name: String,
+    field: Field,
+}
+
+impl User { 
+    fn new(name: String) -> Self {
+        Self { name, field: Field::new() }
+    }
 }
 
 fn main() {
-    let f1 = Field::new();
-    let is_hit = f1.shoot(1, 2);
-    println!("{}", is_hit);
-    println!("{}", f1.ships[0].is_sunk())
+    let u1 = User::new("Cam".into());
+    let u2 = User::new("Maya".into());
 }
 
 // Generate game board
-    // Create new matrix n*n
-    // Get vec of pieces
-    // Choose if piece is vertical or horizontal
-    // 
+// Create new matrix n*n
+// Get vec of pieces
+// Choose if piece is vertical or horizontal
+//
 
-// 5 ships
-// Carrier: 5 holes
-// Battleship: 4 holes
-// Cruiser: 3 holes
-// Submarine: 3 holes
-// Destroyer: 2 holes
-// 
 // Game board: 10x10
 // Ships may be horizontal or vertical
