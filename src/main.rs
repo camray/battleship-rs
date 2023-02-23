df4ad89dbdb03f1f04c0079e02d977b6ac1164ea
Cameron Ray <cameron@cameronray.net>
2023-02-22 20:54:08 -0500

    fix mutability errors

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use std::ptr::null;
+#![allow(dead_code)]
+#![allow(unused_variables)]
 
+#[derive(Debug)]
 struct Point {
     x: u8,
     y: u8,
 }
 
+#[derive(Debug)]
 enum Direction {
     Vertical,
     Horizontal,
 }
 
+#[derive(Debug)]
 struct Ship {
     name: String,
-    size: u8,
     hits: Vec<bool>,
     position: Option<Point>,
+    direction: Option<Direction>,
 }
 
 impl Ship {
@@ -33,46 +37,41 @@ impl Ship {
  * Destroyer: 2 holes
  */
 fn default_ships() -> Vec<Ship> {
-    let mut ships = Vec::new();
-
-    ships.push(Ship {
-        name: "Carrier".into(),
-        size: 5,
-        hits: vec![false; 5],
-        position: None,
-    });
-
-    ships.push(Ship {
-        name: "Battleship".into(),
-        size: 4,
-        hits: vec![false; 4],
-        position: None,
-    });
-
-    ships.push(Ship {
-        name: "Cruiser".into(),
-        size: 3,
-        hits: vec![false; 3],
-        position: None,
-    });
-
-    ships.push(Ship {
-        name: "Submarine".into(),
-        size: 3,
-        hits: vec![false; 3],
-        position: None,
-    });
-
-    ships.push(Ship {
-        name: "Destroyer".into(),
-        size: 2,
-        hits: vec![false; 2],
-        position: None,
-    });
-
-    ships
+    vec![
+        Ship {
+            name: "Carrier".into(),
+            hits: vec![false; 5],
+            position: None,
+            direction: None,
+        },
+        Ship {
+            name: "Battleship".into(),
+            hits: vec![false; 4],
+            position: None,
+            direction: None,
+        },
+        Ship {
+            name: "Cruiser".into(),
+            hits: vec![false; 3],
+            position: None,
+            direction: None,
+        },
+        Ship {
+            name: "Submarine".into(),
+            hits: vec![false; 3],
+            position: None,
+            direction: None,
+        },
+        Ship {
+            name: "Destroyer".into(),
+            hits: vec![false; 2],
+            position: None,
+            direction: None,
+        },
+    ]
 }
 
+#[derive(Debug)]
 struct Field {
     spaces: [[bool; 10]; 10],
     ships: Vec<Ship>,
@@ -93,7 +92,21 @@ impl Field {
         self.spaces[x][y]
     }
 
-    fn place_ship(name: String, position: Point) {}
+    fn place_ship(&mut self, name: String, position: Point, direction: Direction) -> bool {
+        self.ships
+            .iter_mut()
+            .find(|s| s.name == name)
+            .map(|s| {
+                s.position = Some(Point {
+                    x: position.x,
+                    y: position.y,
+                });
+
+                s.direction = Some(direction);
+                true
+            })
+            .unwrap_or(false)
+    }
 }
 
 struct User {
@@ -101,15 +114,30 @@ struct User {
     field: Field,
 }
 
-impl User { 
+impl User {
     fn new(name: String) -> Self {
-        Self { name, field: Field::new() }
+        Self {
+            name,
+            field: Field::new(),
+        }
     }
 }
 
+struct Game {
+    u1: User,
+    u2: User,
+}
+
 fn main() {
-    let u1 = User::new("Cam".into());
+    let mut u1 = User::new("Cam".into());
+    u1.field.place_ship(
+        "Destroyer".into(),
+        Point { x: 1, y: 2 },
+        Direction::Horizontal,
+    );
     let u2 = User::new("Maya".into());
+
+    println!("{:?}", u1.field);
 }
 
 // Generate game board
